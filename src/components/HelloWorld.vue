48df99a4fa2af6548f985d82ad189c35f5ced540
Nelson <nelsonsnake_@hotmail.com>
2019-11-24 10:11:33 -0600

    arreglar pruebas unitarias

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <h1>{{msg}}</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        msg: String
+    },
+    created(){
+        console.log("created function now available");
+    }
+};
+</script>
